a888b526a809efc4b3b78d103248a2bf6244bec2
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2024-06-22 09:59:28 +0330

    fix: update bash

--- send-updates.sh
+++ send-updates.sh
@@ -1,8 +1,10 @@
+declare topic=$1
 for i in $(seq 1 1000);
 do
-   echo "\rpool id = $i http://localhost:3000/webhook"
-    nohup curl "http://localhost:3000/webhook" \
-    --header 'Content-Type: application/json' \   
-    echo "\r"
+   echo "\rpool id = $i http://localhost:3000/webhook/${topic}"
+    curl "http://localhost:3000/webhook/${topic}" \
+    --header 'Content-Type: application/json'   
+   echo "\r"
+   
    
 done
